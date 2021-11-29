e74f5b4fcf825dd51507093f2b8bf40f78de575b
Carly Perdue <carlyperdue@gmail.com>
2021-11-28 20:31:40 -0500

    Implemented more chat features

---
+++ src/src/components/nested/UserMessage.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row>
+    <v-col md="8" offset-md="0">
+      <v-card color="#81dfde" class="chat-bubble">
+        <p class="text--secondary font-weight-light px-2">
+          {{ message }}
+        </p>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: ["message"],
+};
+</script>
+
+<style></style>
